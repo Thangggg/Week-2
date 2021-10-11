47f87dbee54159978eaf383b8aac0724cfe4d0a8
Thangggg <thang.nguyenvan7594@gmail.com>
2021-10-11 11:44:47 +0700

    sudungtoantu

---
+++ Sudungtoantu/sudungtoantu.js
@@ -0,0 +1,6 @@
+let inputWidth =prompt("Nhập chiều cao");
+let inputHeight =prompt("Nhập chiều rộng");
+let width= parseInt(inputWidth);
+let height =parseInt(inputHeight);
+let area = width*height;
+document.write("the area is: "+ area)
